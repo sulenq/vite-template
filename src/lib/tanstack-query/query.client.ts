a11a4917c3edf33f82ffc6e55c8e7e6ac2859fac
sulenq <fatwalinovera@gmail.com>
2026-06-11 08:38:01 +0700

    feat: add tasntack query lib

---
+++ src/lib/tanstack-query/query.client.ts
@@ -0,0 +1,17 @@
+import { QueryClient } from "@tanstack/react-query";
+
+// -----------------------------------------------------------------
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1 * 60 * 1000, // ms => 1 minutes
+      gcTime: 1 * 60 * 1000, // ms => 1 minutes
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
---
+++ src/lib/tanstack-query/query.keys.ts
@@ -0,0 +1,6 @@
+export const queryKeys = {
+  auth: {
+    all: ["auth"],
+    profile: () => ["auth", "profile"],
+  },
+};
