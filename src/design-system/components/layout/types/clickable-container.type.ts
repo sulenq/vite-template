d8bb49e20a948e5fc2dbad583048aadd024c3004
sulenq <fatwalinovera@gmail.com>
2026-07-11 17:51:12 +0700

    feat: add ClickableContainer component for handling label and ref-based interaction patterns

---
+++ src/design-system/components/layout/types/clickable-container.type.ts
@@ -0,0 +1,9 @@
+// src/design-system/components/layout/types/clickable-container.type.ts
+
+import type { StackProps } from "@/design-system/components/layout/types/stack.type";
+
+export type ClickableContainerProps = StackProps & {
+  children: React.ReactNode;
+  targetRef?: React.RefObject<HTMLInputElement>;
+  asLabel?: boolean;
+};
---
+++ src/design-system/components/layout/ui/clickable-container.tsx
@@ -0,0 +1,29 @@
+// src/design-system/components/layout/ui/clickable-container.tsx
+
+import type { ClickableContainerProps } from "@/design-system/components/layout/types/clickable-container.type";
+import { HStack } from "@/design-system/components/layout/ui/flex-box";
+import { chakra } from "@chakra-ui/react";
+
+export const ClickableContainer = (props: ClickableContainerProps) => {
+  const { children, targetRef, asLabel = true } = props;
+
+  if (asLabel) {
+    return (
+      <chakra.label display={"flex"} alignItems={"center"} cursor={"pointer"}>
+        {children}
+      </chakra.label>
+    );
+  }
+
+  return (
+    <HStack
+      cursor={"pointer"}
+      onClick={(e) => {
+        if (targetRef?.current?.contains(e.target as Node)) return;
+        targetRef?.current?.click();
+      }}
+    >
+      {children}
+    </HStack>
+  );
+};
