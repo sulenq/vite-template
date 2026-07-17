241a5adb97b63ad58af6d912b9e5cbee0676a7fb
sulenq <fatwalinovera@gmail.com>
2026-07-17 09:51:24 +0700

    styling

--- src/design-system/components/feedback/ui/state.no-data.tsx
+++ src/design-system/components/feedback/ui/state.no-data.tsx
@@ -5,12 +5,12 @@
 import type { FeedbackStateProps } from "@/design-system/components/feedback/types/feedback-state.type";
 import FeedbackState from "@/design-system/components/feedback/ui/feedback-state";
 import { t } from "@/shared/libs/i18n";
-import { MehIcon } from "lucide-react";
+import { WindIcon } from "lucide-react";
 
 export const NoDataState = (props: FeedbackStateProps) => {
   // Props
   const {
-    icon = MehIcon,
+    icon = WindIcon,
     title = t["common.no_data"](),
     description,
     children,
