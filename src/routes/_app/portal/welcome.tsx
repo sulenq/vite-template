8296163e67dbfb79defcbdc7808a02228f22d579
sulenq <fatwalinovera@gmail.com>
2026-07-29 08:51:45 +0700

    feat: implement welcome portal layout with internationalized introduction text

--- src/routes/_app/portal/welcome.tsx
+++ src/routes/_app/portal/welcome.tsx
@@ -1,5 +1,8 @@
 // src/routes/_app/portal/welcome.tsx
 
+import { VStack } from "@/design-system/components/layout/ui/flex-box";
+import { P } from "@/design-system/components/typography/ui/p";
+import { t } from "@/shared/libs/i18n";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/portal/welcome")({
@@ -7,5 +10,11 @@ export const Route = createFileRoute("/_app/portal/welcome")({
 });
 
 function RouteComponent() {
-  return <div>Portal Welcome</div>;
+  return (
+    <VStack flex={1} align={"center"} justify={"center"} gap={1} p={4}>
+      <P fontSize={"lg"} fontWeight={"medium"} textAlign={"center"}>
+        {t["common.welcome_intro"]()}
+      </P>
+    </VStack>
+  );
 }
